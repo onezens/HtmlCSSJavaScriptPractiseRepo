3fd111dd6511aeb776aa8553ff07c484f173e2ff
wackosix <mail@onezen.cc>
2017-07-06 18:11:06 +0800

    home-image-slider

--- Project/weijinsuo/js/main.js
+++ Project/weijinsuo/js/main.js
@@ -1,3 +1,18 @@
 /**
  * Created by wangzhen on 17/6/26.
  */
+$(function () {
+    function resize() {
+        var winWidth = $(window).width();
+        console.log(winWidth);
+        $('#home_slide .item-image').each(function (i, item) {
+            var $item = $(item);
+            var imgSrc = $item.data(winWidth < 768 ? 'sm-image' : 'lg-image');
+            var imgHtml = '<img src="' + imgSrc + '"/>';
+            $item.html(imgHtml);
+            $item.css('backgroundImage', 'url('+imgSrc+')');
+
+        });
+    }
+    $(window).on('resize', resize).trigger('resize');
+});
